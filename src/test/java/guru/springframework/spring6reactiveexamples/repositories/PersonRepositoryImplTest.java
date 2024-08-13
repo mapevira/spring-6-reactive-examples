08c943254bdbad71ffded9bf85ba6f1d66ba7b61
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-13 09:24:35 +0200

    258. Error Handling

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -93,4 +93,25 @@ class PersonRepositoryImplTest {
 
         fionaMono.subscribe(person -> System.out.println(person.getFirstName()));
     }
+
+    @Test
+    void testFindPersonByIdNotFound() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        final int id = 8;
+
+        Mono<Person> personMono = personFlux.filter(person -> person.getId() == id)
+                .single()
+                .doOnError(throwable -> {
+                    System.out.println("Error occurred in flux");
+                    System.out.println(throwable.toString());
+                });
+
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        }, throwable -> {
+            System.out.println("Error occurred in the mono");
+            System.out.println(throwable.toString());
+        });
+    }
 }
